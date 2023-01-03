89bf723eb0120c02b25d97a4b718a26b25a72e8e
egesenli <egesenli@gmail.com>
2023-01-03 13:53:57 -0500

    Fix bug, add vertical word and empty array cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
     }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = letters[0].map((_, i) => letters.map(ls => ls[i]).join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
